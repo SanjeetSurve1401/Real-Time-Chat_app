ac79eed6f2eec551cc2c1f7bb85a95295aaa768d
sanjusurve1401 <sanjeetsurve1401@gmail.com>
2025-05-07 12:56:01 +0530

    created middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -45,9 +45,50 @@ export const signup = async(req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
-export const login = (req, res) => {
-    res.send("login page");
+
+
+export const login = async (req, res) => {
+    const {email, password} = req.body
+    try {
+        const user = await User.findOne({email})
+
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+        
+        const isPassword = await bcrypt.compare(password, user.password)
+        if(!isPassword){
+            return res.status(400).json({message: "Invalid credentials"})
+        }
+
+        generatToken(user._id,res)
+
+        res.status(200).json({
+            message: "Logged In Successfully",
+            _id: user._id,
+            fullName : user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        })
+
+
+    } catch (error) {
+        console.log("Error in login controller ", error.message);
+        res.status(500).json({message: "Internal Server Error"})        
+    }
 };
 export const logout = (req, res) => {
-    res.send("logout page");
+    try {
+        res.cookie("jwt", "", {maxAge:0})
+        res.status(200).json({message: "Logged out successfully"})
+
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal Server Error"})  
+
+    }
+};
+
+export const updateProfile = async(req, res)=>{
+    
 };
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import { login, logout, signup } from '../controllers/auth.controller.js';    
+import { protectRoute } from '../../middleware /auth.middleware.js';
+
+
 
 const router = express();
 
@@ -8,5 +11,8 @@ router.post("/signup", signup);
 router.post("/login",login);
 
 router.post("/logout",logout);
+
+router.put("/update-profile ",protectRoute, updateProfile) 
+
 
 export default router;
